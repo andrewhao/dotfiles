7a0e7b6bb4afc7ddb74c7471ab192c67fd2236eb
Andrew Hao <andrewhao@gmail.com>
2015-09-01 11:33:29 -0700

    Adding new docker aliases

--- boot2docker/docker-compose.zsh
+++ boot2docker/docker-compose.zsh
@@ -1,2 +1,6 @@
 alias dcp='docker-compose'
+alias dc='docker-compose'
 alias drw='docker-compose run web'
+alias dcrun='docker-compose run web'
+alias dctest='docker-compose run -e RAILS_ENV=test web'
+
