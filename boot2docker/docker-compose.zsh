4a8a61147e8827051b6682fed57881aa799848dd
Andrew Hao <andrew@carbonfive.com>
2016-08-25 16:49:52 -0700

    Adding vim persistent undo shortcut, plus docker-compose exec alias.

--- boot2docker/docker-compose.zsh
+++ boot2docker/docker-compose.zsh
@@ -1,6 +1,7 @@
 alias dcp='docker-compose'
 alias dc='docker-compose'
 alias drw='docker-compose run web'
+alias dea='docker-compose exec app'
 alias dcrun='docker-compose run web'
 alias dctest='docker-compose run -e RAILS_ENV=test web'
 
