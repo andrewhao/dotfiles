41787c940ac467adecc20087e78c6ec9a669061e
Andrew Hao <andrew@carbonfive.com>
2017-10-13 11:35:52 -0700

    Remove powerline-config

--- zsh/powerline.zsh
+++ zsh/powerline.zsh
@@ -1,3 +1,3 @@
 export POWERLINE_PATH="`pip show powerline-status | grep Location | cut -d " " -f 2`/powerline"
 . $POWERLINE_PATH/bindings/zsh/powerline.zsh
-powerline-config tmux source
+# powerline-config tmux source
