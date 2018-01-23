acb8a200d524eeb1379225da440a516c9f40d2e1
Andrew Hao <andrewhao@gmail.com>
2018-01-22 19:52:15 -0800

    Updating zplug config to be less verbose

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -9,7 +9,14 @@ source $ZPLUG_HOME/init.zsh
 source $HOME/.dotfiles/git/aliases.zsh
 
 # List of ZPlug plugins.
+# ----------------------
 zplug denysdovhan/spaceship-prompt, use:spaceship.zsh, from:github, as:theme
+
+# Set the priority when loading
+# e.g., zsh-syntax-highlighting must be loaded
+# after executing compinit command and sourcing other plugins
+# (If the defer tag is given 2 or above, run after compinit command)
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
@@ -20,4 +27,4 @@ if ! zplug check --verbose; then
 fi
 
 # Then, source plugins and add commands to $PATH
-zplug load --verbose
+zplug load
