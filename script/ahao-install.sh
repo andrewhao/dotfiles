acc69e1623c67403288b52a52625c4c960c96752
Andrew Hao <andrew@carbonfive.com>
2018-08-06 11:13:24 -0700

    Adding postinstall script

---
+++ script/ahao-install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# Postinstall scripts
+
+echo "Linking nvim"
+mkdir -p ~/.config
+ln -s ~/.nvim ~/.config/nvim
+echo "Done."
+
+echo "Installing vim-plug..."
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "Done."
+
+echo "Installing App Store packages..."
+mas install 803453959
+echo "Done."
